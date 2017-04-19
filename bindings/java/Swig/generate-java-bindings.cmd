6a391673d24400f87cb6416041720bf8787d9bc5
marhamil723 <mhamilton723@gmail.com>
2017-04-18 20:21:48 -0400

    fixing build script

--- bindings/java/Swig/generate-java-bindings.cmd
+++ bindings/java/Swig/generate-java-bindings.cmd
@@ -1,16 +1,17 @@
 setlocal enableextensions enabledelayedexpansion
-SET SOL_DIR=%~f1
-echo %SOL_DIR%
-if not exist %SOL_DIR%\bindings\java\Swig\com\microsoft\CNTK\ mkdir %SOL_DIR%\bindings\java\Swig\com\microsoft\CNTK\
-%SWIG_PATH%\swig.exe -c++ -java -DMSC_VER -I%SOL_DIR%Source\CNTKv2LibraryDll\API -I%SOL_DIR%bindings\common -package com.microsoft.CNTK -outdir  %SOL_DIR%bindings\java\Swig\com\microsoft\CNTK  %SOL_DIR%bindings\java\Swig\cntk_java.i
+SET sol_dir=%~f1
 
-pushd %SOL_DIR%\bindings\java\Swig
-cd
+echo %sol_dir%
+
+if not exist "%sol_dir%\bindings\java\Swig\com\microsoft\CNTK\" mkdir "%sol_dir%\bindings\java\Swig\com\microsoft\CNTK\"
+"%SWIG_PATH%\swig.exe" -c++ -java -DMSC_VER -I%SOL_DIR%Source\CNTKv2LibraryDll\API -I%sol_dir%bindings\common -package com.microsoft.CNTK -outdir  %sol_dir%bindings\java\Swig\com\microsoft\CNTK  %sol_dir%bindings\java\Swig\cntk_java.i
+
+cd "%sol_dir%\bindings\java\Swig"
+
 REM: TODO: add check whether javac/jar exist.
-echo build java
-%JAVA_HOME%\bin\javac .\com\microsoft\CNTK\*.java
-%JAVA_HOME%\bin\jar -cvf cntk.jar .\com\microsoft\CNTK\*.class
+echo building java
+
+"%JAVA_HOME%\bin\javac" .\com\microsoft\CNTK\*.java
+"%JAVA_HOME%\bin\jar" -cvf cntk.jar .\com\microsoft\CNTK\*.class
 rd com /q /s
-dir cntk.jar
-popd
 
