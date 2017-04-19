2c8eba281dc11d06b90c498a26568ed37facfd6b
marhamil723 <mhamilton723@gmail.com>
2017-04-19 10:57:29 -0400

    fixing comments and tests

--- bindings/java/JavaEvalTest/run-java-test.cmd
+++ bindings/java/JavaEvalTest/run-java-test.cmd
@@ -1,20 +1,18 @@
 @echo off
 setlocal enableextensions enabledelayedexpansion
-SET SOL_DIR=%~dpf1
-SET BUILD_PATH=%~dpf2
+SET sol_dir=%~dpf1
+SET build_path=%~dpf2
 
-SET PATH=%PATH%;%BUILD_PATH%
+SET PATH=%PATH%;%build_path%
 
-pushd "%SOL_DIR%\bindings\java\JavaEvalTest"
-"%JAVA_HOME%\bin\javac" -cp "%SOL_DIR%\bindings\java\Swig\cntk.jar" src\Main.java || (
+cd "%sol_dir%\bindings\java\JavaEvalTest"
+"%JAVA_HOME%\bin\javac" -cp "%sol_dir%\bindings\java\Swig\cntk.jar" src\Main.java || (
   echo "Java Compilation Failed"
-  popd
   EXIT /B 1
 )
-"%JAVA_HOME%\bin\java" -Djava.library.path=%BUILD_PATH% -classpath "%SOL_DIR%\bindings\java\JavaEvalTest\src;%SOL_DIR%\bindings\java\Swig\cntk.jar" Main || (
+"%JAVA_HOME%\bin\java" -Djava.library.path=%build_path% -classpath "%sol_dir%\bindings\java\JavaEvalTest\src;%sol_dir%\bindings\java\Swig\cntk.jar" Main || (
   echo "Running Java Failed"
-  popd
   EXIT /B 1
 )
-popd
+
 
--- bindings/java/JavaEvalTest/src/Main.java
+++ bindings/java/JavaEvalTest/src/Main.java
@@ -2,26 +2,26 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import com.microsoft.CNTK.*;
+
 import javax.imageio.ImageIO;
-import java.awt.image.*;
-import java.awt.Image;
-import java.awt.Color;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
-    public static Boolean equals(double d1,double d2){
-        double sensitivity=.00001;
-        return Math.abs(d1-d2)<sensitivity;
+    private static Boolean equals(float d1, float d2) {
+        double sensitivity = .00001;
+        return Math.abs(d1 - d2) < sensitivity;
     }
-
 
     public static void main(String[] args) throws IOException {
         System.loadLibrary("CNTKLibraryJavaBinding");
 
-        String dataPath = System.getenv("CNTK_EXTERNAL_TESTDATA_SOURCE_DIRECTORY") + "/Bindings/Java/";
-        Function modelFunc = Function.LoadModel(dataPath + "z.model");
+        DeviceDescriptor device = DeviceDescriptor.UseDefaultDevice();
+        File dataPath = new File("data/");
+        Function modelFunc = Function.LoadModel(new File(dataPath, "z.model").getAbsolutePath());
         Variable outputVar = modelFunc.getOutputs().get(0);
         Variable inputVar = modelFunc.getArguments().get(0);
 
@@ -31,13 +31,13 @@ public class Main {
         int imageChannels = inputShape.getDimensions().get(2).intValue();
         int imageSize = ((int) inputShape.GetTotalSize());
 
-        System.out.print("EvaluateSingleImage");
+        System.out.println("EvaluateSingleImage");
 
         // Image preprocessing to match input requirements of the model.
-        BufferedImage bmp = ImageIO.read(new File(dataPath + "00000.png"));
+        BufferedImage bmp = ImageIO.read(new File(dataPath, "00000.png"));
         Image resized = bmp.getScaledInstance(imageWidth, imageHeight, Image.SCALE_DEFAULT);
-        BufferedImage bImg =
-                new BufferedImage(resized.getWidth(null), resized.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        BufferedImage bImg = new BufferedImage(
+                resized.getWidth(null), resized.getHeight(null), BufferedImage.TYPE_INT_RGB);
         // or use any other fitting type
         bImg.getGraphics().drawImage(resized, 0, 0, null);
 
@@ -68,7 +68,7 @@ public class Main {
         FloatVectorVector floatVecVec = new FloatVectorVector();
         floatVecVec.add(floatVec);
         // Create input data map
-        Value inputVal = Value.CreateDenseFloat(inputShape, floatVecVec, DeviceDescriptor.GetCPUDevice());
+        Value inputVal = Value.CreateDenseFloat(inputShape, floatVecVec, device);
         UnorderedMapVariableValuePtr inputDataMap = new UnorderedMapVariableValuePtr();
         inputDataMap.Add(inputVar, inputVal);
 
@@ -78,24 +78,24 @@ public class Main {
         outputDataMap.Add(outputVar, null);
 
         // Start evaluation on the device
-        modelFunc.Evaluate(inputDataMap, outputDataMap, DeviceDescriptor.GetCPUDevice());
+        modelFunc.Evaluate(inputDataMap, outputDataMap, device);
 
         // Get evaluate result as dense output
         FloatVectorVector outputBuffer = new FloatVectorVector();
         outputDataMap.getitem(outputVar).CopyVariableValueToFloat(outputVar, outputBuffer);
 
-        double[] trueResults = {
-                -1.887479, -4.768533,
-                0.1516971, 6.805476,
-                -0.3840595, 3.4106512,
-                1.3302777, -0.87714916,
-                -2.18046, -4.1666183};
+        float[] trueResults = {
+                -1.887479f, -4.768533f,
+                0.1516971f, 6.805476f,
+                -0.3840595f, 3.4106512f,
+                1.3302777f, -0.87714916f,
+                -2.18046f, -4.1666183f};
 
 
         for (int j = 0; j < outputBuffer.get(0).size(); j++) {
             System.out.println(outputBuffer.get(0).get(j) + " ");
-            if (!equals(trueResults[j],outputBuffer.get(0).get(j))){
-                throw new java.lang.RuntimeException("Test Failed on output "+j);
+            if (!equals(trueResults[j], outputBuffer.get(0).get(j))) {
+                throw new RuntimeException("Test Failed on output " + j);
             }
         }
         System.out.println("Evaluation Complete");
