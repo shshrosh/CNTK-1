69ce4922d44bbb7836cd867ed8292b3c13abad8d
Zhou Wang <zhouwang@microsoft.com>
2017-04-26 15:05:05 +0200

    make Java test work on Windows

--- bindings/java/JavaEvalTest/run-java-test.cmd
+++ bindings/java/JavaEvalTest/run-java-test.cmd
@@ -1,20 +1,19 @@
 setlocal enableextensions enabledelayedexpansion
-SET sol_dir=%~dpf1
-SET build_path=%~dpf2
+SET build_path=%~dpf1
 
 SET PATH=%PATH%;%build_path%
 
-if not exist "%sol_dir%\bindings\java\JavaEvalTest\src\Main.java" (
-  echo "The class src\Main.java does not exist"
+if not exist "%build_path%\java\Main.class" (
+  echo "%build_path%\java\Main.class" does not exist!
   EXIT /B 1
 )
 
-if not exist "%sol_dir%\bindings\java\Swig\cntk.jar" (
-  echo "The class cntk.jar does not exist"
+if not exist "%build_path%\java\cntk.jar" (
+  echo "%build_path%\java\cntk.jar" does not exist!
   EXIT /B 1
 )
 
-"%JAVA_HOME%\bin\java" -Djava.library.path=%build_path% -classpath "%sol_dir%\bindings\java\JavaEvalTest\src;%sol_dir%\bindings\java\Swig\cntk.jar" Main || (
-  echo "Running Java Failed"
+"%JAVA_HOME%\bin\java" -Djava.library.path=%build_path% -classpath "%build_path%\java;%build_path%\java\cntk.jar" Main || (
+  echo Running Java test failed!
   EXIT /B 1
 )
