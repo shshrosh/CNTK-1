39e74d1ccdd165522dbb24be36368808756de252
Zhou Wang <zhouwang@microsoft.com>
2017-04-26 15:05:05 +0200

    make Java test work on Windows

--- bindings/java/JavaEvalTest/run-java-test.sh
+++ bindings/java/JavaEvalTest/run-java-test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
-CNTK_PATH="$1"
-LIB_PATH="$2"
-cd $CNTK_PATH/bindings/java/JavaEvalTest
-javac -cp $CNTK_PATH/bindings/java/Swig src/Main.java
+LIB_PATH="$1"
+
 export LD_LIBRARY_PATH=$LIB_PATH:$LD_LIBRARY_PATH
-java -classpath "$CNTK_PATH/bindings/java/JavaEvalTest/src:$CNTK_PATH/bindings/java/Swig" Main
+java -classpath "$LIB_PATH/java" Main
